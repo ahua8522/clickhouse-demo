abdb595cbee2959151a7a9c3f2884e0da6f7e51c
wanqh <wanqh@bsfit.com.cn>
2019-10-09 20:57:23 +0800

    add test

--- src/main/java/cn/com/wanqh/demo/clickhouse/ClickhouseApplication.java
+++ src/main/java/cn/com/wanqh/demo/clickhouse/ClickhouseApplication.java
@@ -1,9 +1,15 @@
 package cn.com.wanqh.demo.clickhouse;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"cn.com.wanqh.demo.clickhouse.mapper"})
+@ComponentScan(basePackages = {"cn.*", "com.*", "net.*"})
 public class ClickhouseApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/com/wanqh/demo/clickhouse/mapper/UisTestMapper.java
@@ -0,0 +1,11 @@
+package cn.com.wanqh.demo.clickhouse.mapper;
+
+/**
+ * @author: bangsun
+ * @date: created in 2019/10/9 20:52
+ * @version:
+ * @since:
+ */
+public interface UisTestMapper {
+    int count();
+}
